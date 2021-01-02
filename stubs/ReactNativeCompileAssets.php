d7ff050e922b79ff99426d48a0f08a7c65c2fef5
Fred-QS <66546741+Fred-QS@users.noreply.github.com>
2021-01-02 13:52:51 +0100

    update

--- stubs/ReactNativeCompileAssets.php
+++ stubs/ReactNativeCompileAssets.php
@@ -5,23 +5,22 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use Quantic\ReactNative\Commands\Listener;
 
-class ReactNativeCreatePage extends Command
+class ReactNativeCompileAssets extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'reactnative:create 
-                            {file? : File to create}
-                            {--type=null : Document type}';
+    protected $signature = 'reactnative:compile 
+                            {extension : File type .css or .js}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Create page component for ReactNativeCreatePage App compilation from Blade & Tailwind';
+    protected $description = 'Compile CSS files or JS files where sources come from config(\'reactnative.css\') or config(\'reactnative.js\')';
 
     /**
      * Create a new command instance.
@@ -40,12 +39,11 @@ class ReactNativeCreatePage extends Command
      */
     public function handle(): void
     {
-        $arg = $this->argument('file');
-        $option = $this->option('type');
+        $arg = $this->argument('extension');
         $command = $this->argument('command');
         $method = (strpos($command, ':') > 0) ? explode(':', $command)[1] : '';
 
         // Execute Quantical Solutions ReactNative command
-        new Listener($this, $method, $arg, $option);
+        new Listener($this, $method, $arg, false);
     }
 }
