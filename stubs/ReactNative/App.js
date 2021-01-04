7851736aa85102efa7017cadaf0c8dbbfac850d3
Fred-QS <66546741+Fred-QS@users.noreply.github.com>
2021-01-04 20:07:31 +0100

    Update App.js

--- stubs/ReactNative/App.js
+++ stubs/ReactNative/App.js
@@ -1,21 +1,39 @@
 import React, { Component } from 'react';
-import {StatusBar, View, StyleSheet} from 'react-native';
-import {Url} from './includes/components/Url';
+import {StatusBar, View, StyleSheet, Text} from 'react-native';
+import Logo from "../../../../../app/ReactNative/assets/not-connected.svg";
 
 export default class App extends Component {
 
     render() {
         return (
-            <View style={styles.container}>
-                <StatusBar hidden={false} backgroundColor={'#FFF'} barStyle={'dark-content'} />
-                <Url />
+            <View style={styles.view}>
+                <Logo style={styles.logo} />
+                <Text style={styles.text}>Ready to play ?</Text>
+                <Text style={styles.small}>
+                    Welcome to your Laravel - ReactNative Package</Text>
             </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1
+    view: {
+        flex: 1,
+        backgroundColor: '#F7F7F7',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    text: {
+        color: 'black',
+        fontWeight: 'bold',
+        fontSize: 30,
+    },
+    small: {
+        color: 'gray',
+        fontSize: 14,
+    },
+    logo: {
+        width: 80,
+        height: 80
     }
 });
